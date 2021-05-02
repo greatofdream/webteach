d057dca4e613094593146876e4600c49f298cb52
greatofdream <greatofdream@gmail.com>
2021-05-02 15:02:36 +0800

    init the configuration

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  title: 'web基础 & DWF',
+  description: 'DWF',
+  base: '/webdwf/',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/'},
+      { text: 'Guide', 
+        items: [
+          { text: 'web基础', link: '/guide/web/'},
+          { text: 'DWF', link: '/guide/dwf/'}
+        ]
+      },
+      { text: 'Example', link: '/examples/'}
+    ],
+    sidebar: {
+          '/guide/web/': [
+            '',
+            'webBrowser',
+            'vue',
+            'springboot'
+          ],
+          '/guide/dwf/': [
+            '',
+            'part1',
+            'part2',
+            'part3',
+          ],
+          '/guide/': [
+            ''
+          ],
+          '/': [
+            '',
+            'about'
+          ],
+        }
+  }
+}
