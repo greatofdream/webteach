8d5d969c176f39b64207b89030f10aa9d237a6a0
greatofdream <greatofdream@gmail.com>
2023-04-04 14:40:07 +0800

    v1->v2: config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,37 @@
-module.exports = {
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+//import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+export default defineUserConfig({
   title: '量子数据分析云',
   description: 'DWF',
   base: '/',
   record: "京ICP备2020037097号-1",
   year: "2023/01/14",
-  themeConfig: {
-    nav: [
+  theme: defaultTheme({
+    navbar: [
       { text: 'Home', link: '/'},
       { text: 'DWF', 
-        items: [
+        children: [
           { text: 'web基础', link: '/guide/web/'},
           { text: 'DWF', link: '/guide/dwf/'}
         ]
       },
       { text: '前端', 
-	items: [
+	children: [
 	  { text: '面试经验', link: '/front/experience/'},
 	  { text: 'Node', link: '/front/Node/'},
 	  { text: 'Webpack&Babel', link: '/front/Webpack/' },
 	]
       },
       { text: '后端',
-	items: [
+	children: [
 	  { text: 'Typescript', link: '/backend/Typescript/' },
 	]
       },
       {
         text: '深度学习',
 	link: '/DeepLearning/',
-	items: []
+	children: []
       }
     ],
     sidebar: {
@@ -80,5 +83,5 @@ module.exports = {
             'about'
           ],
         }
-  }
-}
+  })
+})
